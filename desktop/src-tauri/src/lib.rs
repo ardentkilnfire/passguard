536041584f5a1d434734f293bc73108e1286a371
Anil Kumar <akt01.dev@gmail.com>
2024-04-08 14:03:52 +0530

    feat: add temporary login system

--- desktop/src-tauri/src/lib.rs
+++ desktop/src-tauri/src/lib.rs
@@ -1,6 +1,20 @@
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
+
+use tauri::Manager;
+
 pub fn run() {
   tauri::Builder::default()
+  .invoke_handler(tauri::generate_handler![process_secret_key])
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
+}
+
+#[tauri::command]
+async fn process_secret_key(window: tauri::Window, key: String) {
+
+  // Example validation (replace with actual logic)
+  let is_valid = key == "01$a06*";
+
+  // Emitting an event back to the frontend with the validation result
+  window.emit("secret_key_validation", &is_valid).expect("Failed to emit event");
 }
--- desktop/src/routes/login/+page.svelte
+++ desktop/src/routes/login/+page.svelte
@@ -1,29 +1,88 @@
 <script lang="ts">
+	import { invoke } from '@tauri-apps/api/core';
+	import { listen } from '@tauri-apps/api/event';
 	import { goto } from '$app/navigation';
 	import { Routes } from '$lib/config';
+
+	let SECRET_KEY: string;
+
+	async function verifyKeyWithBackend(key: string) {
+		try {
+			// Invoke a command named 'process_secret_key' and send the secret key to the backend
+			setupValidationListener();
+			await invoke('process_secret_key', { key });
+		} catch (error) {
+			alert(`Failed to send the secret key: ${error}`);
+		}
+	}
+
+	// Function to setup event listener for the secret key validation result
+	function setupValidationListener() {
+		listen('secret_key_validation', (event) => {
+			if (event.payload) {
+				goto(Routes.APP_ROOT);
+			} else {
+				alert('Secret key is invalid.');
+				// Handle invalid key scenario
+			}
+		});
+	}
 </script>
 
-<button
-	on:click={() => {
-		alert('Not implemented yet');
-		// Temporary bypass
-		goto(Routes.APP_ROOT);
-	}}
->
-	Open Login Dialogue
-</button>
+<div class="container">
+	<div class="title">
+		<p>Login</p>
+		<div class="divider" />
+	</div>
+
+	<input
+		type="password"
+		name="SECRET_KEY"
+		placeholder="Enter your SECRET_KEY"
+		bind:value={SECRET_KEY}
+	/>
+
+	<button on:click={() => verifyKeyWithBackend(SECRET_KEY)}>Login</button>
+</div>
 
 <style>
-	button {
-		background-color: rgba(var(--primary-rgb), 0.2);
-		padding: var(--spacing-8) var(--spacing-16);
+	.container {
+		display: flex;
+		width: 62rem;
+		max-width: 62rem;
+		flex-direction: column;
+		background-color: rgba(255, 255, 255, 0.05);
 		border-radius: var(--rounded-4);
+		gap: var(--spacing-16);
+		padding: var(--spacing-16);
 		position: absolute;
 		top: 50%;
 		left: 50%;
 		transform: translate(-50%, -50%);
+	}
+
+	.title {
+		display: flex;
+		flex-direction: column;
+		gap: var(--spacing-8);
+	}
+
+	.divider {
+		height: 1px;
+		background-color: rgba(255, 255, 255, 0.05);
+	}
+
+	.container > input {
+		background-color: rgba(0, 0, 0, 0.25);
+		padding: var(--spacing-8);
+		border-radius: var(--rounded-4);
+	}
+
+	.container > button {
+		background-color: var(--primary);
+		color: var(--bg);
+		padding: var(--spacing-8) 0;
+		border-radius: var(--rounded-4);
 		font-weight: 600;
-		color: rgba(var(--primary-rgb), 0.5);
-		box-shadow: var(--shadow-sm);
 	}
 </style>
