72ea0adc56aaf1da17f49f822ac9027ee5c2bb50
Anil Kumar <akt01.dev@gmail.com>
2024-04-12 16:29:22 +0530

    feat: add utility function to fetch website's favicon via google's free api

--- desktop/src/lib/utils/index.ts
+++ desktop/src/lib/utils/index.ts
@@ -2,3 +2,7 @@ export function containsPath(container: string | null | undefined, containment: 
 	if (container) return container.includes(containment);
 	return false;
 }
+
+export function fetchFaviconFromURL(domain: string, size: number | string): string {
+	return `https://www.google.com/s2/favicons?domain=${domain}&sz=${size}`;
+}
